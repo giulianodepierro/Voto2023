8d3c3a105a50718f29b3e5751252fd2b4bedc625
giulianodepierro <53575513+giulianodepierro@users.noreply.github.com>
2023-10-07 10:01:12 -0300

    Projecto en marcha, index Mapeado

--- src/main/java/junin/gob/ar/web/ControladorInicio.java
+++ src/main/java/junin/gob/ar/web/ControladorInicio.java
@@ -3,6 +3,7 @@ package junin.gob.ar.web;
 //import javax.validation.Valid;
 
 //import junin.gob.ar.servicio.PadronService;
+import junin.gob.ar.dao.PadronDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,15 +13,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class ControladorInicio {
-//
-//    @Autowired
-//    private PadronService padronService;
-//
-//    @GetMapping("/")
-//    public  String inicio(Model model){
-//        var padrones = padronService.listarPadron();
-//        model.addAttribute("padrones",padrones);
-//    return "index";
-//    }
 
+    @Autowired
+    private PadronDao padronDao;
+
+
+    @GetMapping("/")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/index")
+    public String hola(Model model) {
+
+        int dniTipo = 1;
+
+        model.addAttribute("mensaje", "Hola desde thymeleaf");
+        model.addAttribute("dni", dniTipo);
+
+        var padrones = padronDao.getPadron();
+        model.addAttribute("padrones", padrones);
+
+//            var tipo =dniDao.getDniTipo();
+//            model.addAttribute("tipo",tipo);
+
+        return "index";
+
+    }
 }
